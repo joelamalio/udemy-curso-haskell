314ea5caf583aea9a07d0c15ed10097706155bab
Joel Amálio <joel.amalio@gmail.com>
2019-01-02 23:34:46 -0300

    Implementando vetores.

---
+++ aula27/teste.hs
@@ -0,0 +1,6 @@
+import Data.Array
+
+get_array = array (1, 4) [(1, 'A'), (2, 'B'), (3, 'C'), (4, 'D')]
+
+-- matriz 2x2
+get_array2 = array ((1, 1), (2,2)) [((1,1), 'A'), ((1,2), 'B'), ((2,1), 'C'), ((2,2), 'D')]
